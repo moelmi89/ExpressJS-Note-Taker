dc7c2f50305b56285628ce9ffae5d1b9deca4cd0
Moelmi89 <moelmi89@gmail.com>
2023-02-26 19:42:42 +0000

    notes html route set up

--- server.js
+++ server.js
@@ -2,6 +2,19 @@
  const express = require("express");
  const fs = require("fs");
 
-
+//set express server
 const app = express();
+
+app.use(express.json());
+app.use(express.static("public"))
+
+
+//added port
 const PORT = process.env.PORT || 3001;
+
+
+
+//routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainPath, "notes.html"));
+});
